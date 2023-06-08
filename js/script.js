6f20b4bd76d0e4e43b2823ed13f1da0c6028717d
gazes12 <100185016+gazes12@users.noreply.github.com>
2023-06-08 04:28:43 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,39 +1,4 @@
 $(document).ready(function() {
-	$(".owl-carousel").owlCarousel({
-		items: 1,
-		loop: true,
-		autoPlay: true,
-		center: true,
-		nav: true,
-		navText: ['<div class="mobile-gallery__prev"><i class="fa-solid fa-backward"></i></div>', '<div class="mobile-gallery__next"><i class="fa-solid fa-forward"></i></div>']
-	});
-
-    
-	$("#prev").click(function() {
-		owl.trigger("owl.prev");
-	});
-	$("#next").click(function() {
-		owl.trigger("owl.next");
-	});
-
-	$(window).resize(function() {
-		checkSizeWindow();
-	});
-	checkSizeWindow();
-
-	function checkSizeWindow() {
-		if($(window).width() > 768) {
-			$(".owl-carousel").trigger('destroy.owl.carousel');
-		} else {
-			$(".owl-carousel").owlCarousel({
-				items: 1,
-			});
-		}
-	}
-
-
-
-
 	let timer_closing;
 
 	function look_info(object, [text, text_type], hide) {
